88c7b712875a26d07f7eabde6f4ca6192058c64f
Ans Christy <jovanacarmel@gmail.com>
2025-12-27 22:19:26 +0530

    refactor: changed from append to write mode

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,28 @@ use std::fs::OpenOptions;
 //TODO MUST: IF CURRENT TASK LIST LENGTH > 0, REMIND USER IF HE CLICKS EXIT
 
 fn decorate() {
-    println!("{} {}{} {}", "\n                 MY".bright_red(), "TO".bright_cyan(), "DO".bright_yellow(), "LIST".bright_red());
-    println!("{}", "add, list, done, delete, search, save, load, exit".purple().italic())
+    println!("{} {}{} {}", "\n               MY".bright_red(), "TO".bright_cyan(), "DO".bright_yellow(), "LIST".bright_red());
+    println!("{}", "add, show, done, delete, search, save, exit".purple().italic())
 }
 
-fn open_file() -> io::Result<std::fs::File> {
+fn open_file_read() -> io::Result<std::fs::File> {
     OpenOptions::new()
         .read(true)
-        .append(true)
+        .write(true)
+        .create(true)
+        .open("todolist.txt")
+}
+
+fn open_file_write() -> io::Result<std::fs::File> {
+    OpenOptions::new()
+        .write(true)
+        .truncate(true)
         .create(true)
         .open("todolist.txt")
 }
 
 fn read_file() -> io::Result<Vec<String>> {
-    let mut file = open_file()?;
+    let mut file = open_file_read()?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
@@ -27,8 +35,8 @@ fn read_file() -> io::Result<Vec<String>> {
     Ok(lines)
 }
 
-fn write_file(list: &Vec<String>) -> io::Result<()>{
-    let mut file = open_file()?;
+fn write_file(list: &Vec<String>) -> io::Result<()> {
+    let mut file = open_file_write()?;
     
     for task in list {
         writeln!(file, "{}", task)?;
@@ -37,38 +45,51 @@ fn write_file(list: &Vec<String>) -> io::Result<()>{
     Ok(())
 }
 
-fn save(list: &mut Vec<String>) {
-    if list.len() <= 0 {
+fn load_into_list(unsaved_tasks_index: &mut usize) -> Vec<String> {
+    match read_file() {
+        Ok(loaded_list) => {
+            if !loaded_list.is_empty() {
+                *unsaved_tasks_index = loaded_list.len() - 1;
+            }
+
+            return loaded_list;
+        }
+        
+        Err(e) => {
+            println!("{}", format!("Error: {e}").bright_red());
+            Vec::new() //return an empty list
+        }
+    }
+}
+
+fn save(list: &mut Vec<String>, unsaved_tasks_index: &mut usize) {
+    if list.len() == 0 {
         println!("List Empty");
         return ();
     }
 
     match write_file(list) {
         Ok(()) => {
+            *unsaved_tasks_index = list.len() - 1;
             println!("{}", "Save Successful".bright_green());
-            list.clear();
-        } 
+        }
         
         Err(e) => println!("{}", format!("Error: {e}").bright_red())
     }
 }
 
-fn show_tasks() {
-    match read_file() {
-        Ok(loaded_list) => {
-            show_list(&loaded_list);
-        } 
-        
-        Err(e) => println!("{}", format!("Error: {e}").bright_red())
-    }
-}
-
-fn show_list(list: &Vec<String>) {
+fn show_list(list: &Vec<String>, unsaved_tasks_index: &usize) {
     if list.is_empty() {
         println!("List Empty, type {} to add tasks", "add <task>".bright_cyan());
     } else {
-        for i in 0..list.len() {
-            println!("{}{} {}", (i+1).to_string().bright_blue(), ".".bright_blue(), list[i]);
+        for i in 0..*unsaved_tasks_index+1 {
+            let line = format!("{}. {}", i + 1, list[i]);
+            println!("{}", line.bright_green());
+        }
+
+        for i in *unsaved_tasks_index+1..list.len(){
+            let line = format!("{}. {}", i + 1, list[i]);
+            println!("{}", line.bright_red());
         }
     }
 }
@@ -81,69 +102,57 @@ fn single_line_add(tasks: &mut Vec<String>, command: &str) {
 
 fn multi_line_add(tasks: &mut Vec<String>) {
     let mut new_task = String::new();
-    io::stdin()
-        .read_line(&mut new_task)
-        .expect("Failed to read task");
-    
+    io::stdin().read_line(&mut new_task).expect("Failed to read task");
     tasks.push(new_task.trim().to_string());
 }
 
-fn is_positive_int(s: &str) -> bool {
-    match s.parse::<u64>() {
-        Ok(n) => n > 0,
-        Err(_) => false
-    }
-}
-
-fn mark_done(index: usize) {
-    match read_file() {
-        Ok(mut loaded_list) => {
-            if index <= 0 || index > loaded_list.len() {
-                println!("{}", "Not valid task number".bright_red());
-            } else {
-                loaded_list.remove(index-1);
-                println!("Task Marked Done!");
-            }
-        } 
+// fn mark_done(index: usize) {
+//     match read_file() {
+//         Ok(mut loaded_list) => {
+//             if index <= 0 || index > loaded_list.len() {
+//                 println!("{}", "Not valid task number".bright_red());
+//             } else {
+//                 loaded_list.remove(index-1);
+//                 println!("Task Marked Done!");
+//             }
+//         } 
         
-        Err(e) => println!("{}", format!("Error: {e}").bright_red())
-    }
-}
+//         Err(e) => println!("{}", format!("Error: {e}").bright_red())
+//     }
+// }
 
-fn single_line_done(command: &str) {
-    let number = command.strip_prefix("done ").expect("Failed to strip prefix 'done'");
+// fn single_line_done(command: &str) {
+//     let number = command.strip_prefix("done ").expect("Failed to strip prefix 'done'");
 
-    match number.trim().parse::<usize>() {
-        Ok(int_number) if int_number > 0 => mark_done(int_number),
-        _ => println!("{}", "Task index not a number".bright_red())
-    }
-}
+//     match number.trim().parse::<usize>() {
+//         Ok(int_number) if int_number > 0 => mark_done(int_number),
+//         _ => println!("{}", "Task index not a number".bright_red())
+//     }
+// }
 
-fn multi_line_done() {
-    show_tasks();
+// fn multi_line_done() {
+//     show_tasks();
     
-    print!("{}", "Enter task number to mark done: ".bright_yellow());
-    io::stdout().flush().expect("Unable to flush stdout");
+//     print!("{}", "Enter task number to mark done: ".bright_yellow());
+//     io::stdout().flush().expect("Unable to flush stdout");
     
-    let mut number = String::new();
-    io::stdin().read_line(&mut number).expect("Unable to read number");
+//     let mut number = String::new();
+//     io::stdin().read_line(&mut number).expect("Unable to read number");
     
-    match number.trim().to_string().parse::<usize>() {
-        Ok(int_number) if int_number > 0 => mark_done(int_number),
-        _ => println!("{}", "Task index not a number".bright_red())
-    }
-}
+//     match number.trim().to_string().parse::<usize>() {
+//         Ok(int_number) if int_number > 0 => mark_done(int_number),
+//         _ => println!("{}", "Task index not a number".bright_red())
+//     }
+// }
 
-fn exit_program(list: &Vec<String>) {
-    if list.len() > 0 {
+fn exit_program(list: &Vec<String>, unsaved_tasks_index: &usize) {
+    if *unsaved_tasks_index < list.len() - 1 {
         let mut choice = String::new();
 
         print!("{}", "You have unsaved tasks, quit anyway? (Y/N) ".bright_red());
         io::stdout().flush().expect("Could not flush out stdout");
 
-        io::stdin()
-            .read_line(&mut choice)
-            .expect("Could not read choice input");
+        io::stdin().read_line(&mut choice).expect("Could not read choice input");
         
         if choice.trim().eq_ignore_ascii_case("Y") {
             exit(0);
@@ -155,7 +164,8 @@ fn exit_program(list: &Vec<String>) {
 fn main() {
     decorate();
 
-    let mut tasks: Vec<String> = Vec::new();
+    let mut unsaved_tasks_index = 0;
+    let mut tasks = load_into_list(&mut unsaved_tasks_index);
 
     let mut input = String::new();
     loop {
@@ -164,30 +174,25 @@ fn main() {
         print!("{}", "\ntodo> ".bright_green());
         io::stdout().flush().expect("Failed to flush stdout");
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read command");
+        io::stdin().read_line(&mut input).expect("Failed to read command");
 
-        
         let command = input.trim();
 
         //one-liner tasks
         if command.starts_with("add ") {
             single_line_add(&mut tasks, command);
         } else if command.starts_with("done ") {
-            single_line_done(command);
+            // single_line_done(command);
         } else {
             //multi-line tasks
             match command {
                 "add" => multi_line_add(&mut tasks),
 
-                "list" => show_list(&tasks),
-
-                "load" => show_tasks(),
+                "show" => show_list(&tasks, &unsaved_tasks_index),
                 
-                "save" => save(&mut tasks),
+                "save" => save(&mut tasks, &mut unsaved_tasks_index),
 
-                "exit" => exit_program(&tasks),
+                "exit" => exit_program(&tasks, &unsaved_tasks_index),
 
                 _ => println!("{}", "Error: Enter valid command\n".bright_red().italic())
             }
